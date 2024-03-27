116fe108a01235e2a049ae7fa7a0283f8b88260f
Denis Jakusjev <theflintgithub@gmail.com>
2024-03-27 22:09:01 +0100

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,10 @@ import NextHead from "@/components/Head";
 import AppBody from "@/components/Body";
 import NavBar from "@/components/Nav";
 import Hero from "@/components/Hero";
+
 const Home = (props: { props: AppProps["pageProps"], name: string }) => {
     return (
-        <AppBody>
-            <NextHead title="Denis Jakusjev Portfolio"/>
-            <NavBar/>
-            <Hero/>
-        </AppBody>)
+        <div>hello</div>)
 };
 
 Home.getInitialProps = async (ctx: NextPageContext) => {
