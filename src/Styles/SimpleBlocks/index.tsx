ecf67d80901d7ee882af8186428c4ce5716372a9
Denis Jakusjev <theflintgithub@gmail.com>
2024-03-28 02:24:00 +0100

    changed import

--- src/Styles/SimpleBlocks/index.tsx
+++ src/Styles/SimpleBlocks/index.tsx
@@ -1,5 +1,5 @@
 import styled, {createGlobalStyle} from "styled-components";
-import Box from "@/components/blocks/Box";
+import Box from "../../components/blocks/Box";
 export const GlobalStyle = createGlobalStyle`
     * {
         margin: 0;
