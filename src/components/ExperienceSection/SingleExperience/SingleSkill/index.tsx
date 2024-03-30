bf3fbdd546b2fe4a5b853afe5cfda0cba4a416b7
Denis Jakusjev <theflintgithub@gmail.com>
2024-03-30 03:53:14 +0100

    Exp desc fixes

--- src/components/ExperienceSection/SingleExperience/SingleSkill/index.tsx
+++ src/components/ExperienceSection/SingleExperience/SingleSkill/index.tsx
@@ -4,8 +4,8 @@ import {JTText} from "@/Styles/textBlocks";
 
 const SingleSkill = ({skillName}:{skillName:string}) => {
     return (
-        <Box border={`1px solid ${getColor("mainGreen")}`} borderRadius={"8px"} width={"max-content"} p={"4px"}>
-            <JTText color={"mainGreen"} fontSize={"12"}>{skillName}</JTText>
+        <Box fontSize={["12px"]} border={`1px solid ${getColor("mainGreen")}`} borderRadius={"8px"} width={"max-content"} p={"4px"}>
+            <JTText color={"mainGreen"} >{skillName}</JTText>
         </Box>
     );
 };
--- src/components/ExperienceSection/SingleExperience/index.tsx
+++ src/components/ExperienceSection/SingleExperience/index.tsx
@@ -9,12 +9,12 @@ import SingleSkill from "@/components/ExperienceSection/SingleExperience/SingleS
 const SingleExperience = ({item, last}: { item: SEType, last?:boolean }) => {
     return (
         <Box as={"section"} display={"flex"} mb={"20px"} width={"100%"}>
-            <Box width={"64px"} display={"flex"} flexDirection={"column"} alignItems={"center"}>
+            <Box width={"64px"} display={["none","flex"]} flexDirection={"column"} alignItems={"center"}>
                 <Image src={item.icon.src} alt={`${item.companyName} logo image`} width={64} height={64}
                        style={{borderRadius: "8px"}}/>
                 {!last ? <Box mt={"5px"} width={"2px"} backgroundColor={getColor("lightGray")} height={"100%"}/> : null}
             </Box>
-            <Box ml={"20px"}>
+            <Box ml={["0px","20px"]}>
                     <Link href={item.link} target={"_blank"}>
                         <JTText fontSize={"18px"} lineHeight={"27px"}><Span
                             color={"mainGreen"}>{item.position}</Span> • {item.companyName}</JTText>
@@ -22,8 +22,8 @@ const SingleExperience = ({item, last}: { item: SEType, last?:boolean }) => {
                 <Box>
                     <INText fontSize={"14px"} lineHeight={"21px"} color={"lightGray"}>{`${item.startDate} - ${item.endDate}`}</INText>
                 </Box>
-                <Box as={"article"} mb={"20px"}>
-                    <INText fontSize={"16px"} lineHeight={"1.6"} fontWeight={"100"}>{item.description}</INText>
+                <Box as={"article"} mb={"20px"} fontSize={["12px","16px"]} lineHeight={"1.6"} fontWeight={["200","100"]}>
+                    <INText  >{item.description}</INText>
                 </Box>
                 <Box display={"flex"} gridGap={"10px"} flexWrap={"wrap"}>
                     {item.techStack.map((skill, index) => (
