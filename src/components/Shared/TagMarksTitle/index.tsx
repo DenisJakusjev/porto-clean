5c95807ee38f25ac5d3889626037431ecddfb2e4
Denis Jakusjev <theflintgithub@gmail.com>
2024-03-30 23:56:09 +0100

    woops

--- src/components/Shared/TagMarksTitle/index.tsx
+++ src/components/Shared/TagMarksTitle/index.tsx
@@ -2,13 +2,14 @@ import {ReactNode} from "react";
 import {JTText, Span} from "@/Styles/textBlocks";
 import Box from "@/components/Blocks/Box";
 
-const TagMarksTitle = ({children, fontSize, lineHeight}: {
+const TagMarksTitle = ({children, fontSize, lineHeight,noMarginBottom}: {
     children: ReactNode,
     fontSize: string | string [],
     lineHeight: string | string []
+    noMarginBottom?: boolean;
 }) => {
     return (
-        <Box fontSize={fontSize} lineHeight={lineHeight} mb={["20px","40px"]}>
+        <Box fontSize={fontSize} lineHeight={lineHeight} mb={noMarginBottom ? "0px" : ["20px","40px"]}>
             <JTText>
                 <Span letterSpacing={"5px"} color={"lightGray"}>{`<`}</Span>
                 {children}
