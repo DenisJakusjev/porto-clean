4c598e7f574abdea6cf8ee723530cbc2897ec8d9
Denis Jakusjev <theflintgithub@gmail.com>
2024-03-30 14:07:34 +0100

    SSR with styled components , removed global

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
 };
 
 export default nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import type {AppProps} from "next/app";
 import {StyleSheetManager} from "styled-components";
 import isPropValid from "@emotion/is-prop-valid";
-
 export default function App({Component, pageProps,}: AppProps) {
     return(
     <StyleSheetManager
