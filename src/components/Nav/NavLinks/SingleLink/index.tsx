6cf21b478d5d3bb82a79da43c3d5520af39a42bb
Denis Jakusjev <theflintgithub@gmail.com>
2024-04-02 19:04:56 +0200

    some shallowRoute

--- src/components/Nav/NavLinks/SingleLink/index.tsx
+++ src/components/Nav/NavLinks/SingleLink/index.tsx
@@ -2,13 +2,21 @@ import {SingleNavItem} from "@/components/Nav/NavLinks/Data";
 import {ScrollToLink} from "@/components/Nav/styles/navStyles";
 import Box from "@/components/Blocks/Box";
 import {JTText, Span} from "../../../../Styles/textBlocks";
+import {shallowRoute} from "@/helpers/Router";
+import {useRouter} from "next/router";
 
 const SingleNavLink = ({title, link}: SingleNavItem) => {
-    const handleClick = () => {
+    const router = useRouter()
+    const handleClick = async () => {
         const Element = document.getElementById(link);
+
         if (Element) {
             Element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+            setTimeout(() => {
+                router.push(`#${link}`);
+            },1000);
         }
+
     }
 
     return (
